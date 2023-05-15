61ddf0feebe096c4463ee3ba0690491b50635432
junejune93 <hjs1597@gmail.com>
2023-05-15 16:03:58 +0900

    make clean

--- p238_ledkey_array/.ledkey_array_dev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/udooer/dd/p238_ledkey_array/ledkey_array_dev.ko := arm-linux-gnueabihf-ld -EL -r  -T /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/scripts/module-common.lds --build-id  -o /home/udooer/dd/p238_ledkey_array/ledkey_array_dev.ko /home/udooer/dd/p238_ledkey_array/ledkey_array_dev.o /home/udooer/dd/p238_ledkey_array/ledkey_array_dev.mod.o
--- p238_ledkey_array/.tmp_versions/ledkey_array_dev.mod
+++
@@ -1,2 +0,0 @@
-/home/udooer/dd/p238_ledkey_array/ledkey_array_dev.ko
-/home/udooer/dd/p238_ledkey_array/ledkey_array_dev.o
