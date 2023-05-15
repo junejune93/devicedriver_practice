0fdd3b51760d068a2a58627c7a1d27bc7f51ab76
junejune93 <hjs1597@gmail.com>
2023-05-15 16:02:21 +0900

    change data type(array)

--- p238_ledkey/ledkey_app.c
+++ p238_ledkey/ledkey_app.c
@@ -12,32 +12,44 @@ void print_led(unsigned char);
 void print_key(unsigned char);
 int main(int argc,char * argv[])
 {
-    int dev;
-    char buff = 0;
-    int ret;
-	if(argc < 2)
-	{
+	int dev;
+	char buff = 0;
+	char oldBuff = 0;
+	int ret;
+	if(argc < 2){
 		printf("USAGE : %s [ledval] \n",argv[0]);
 		return 1;
 	}
 	buff = atoi(argv[1]);
 
-    dev = open( DEVICE_FILENAME, O_RDWR|O_NDELAY );
-	if(dev<0)
-	{
+	dev = open( DEVICE_FILENAME, O_RDWR|O_NDELAY );
+	if(dev<0){
 		perror("open()");
-		return 1;
+		return 2;
 	}
-    ret = write(dev,&buff,sizeof(buff));
-	print_led(buff);
-	if(ret < 0)
+	ret = write(dev,&buff,sizeof(buff));
+	if(ret < 0){
 		perror("write()");
-	
+		return 3;
+	}
+	print_led(buff);
+
 	buff = 0;
-	ret = read(dev,&buff,sizeof(buff));
-	print_key(buff);
-    close(dev);
-    return 0;
+	do {
+		read(dev,&buff,sizeof(buff));
+		if ((buff != 0)&&(oldBuff != buff)){
+			printf("key : %d\n",buff);
+			print_key(buff);
+			write(dev,&buff,sizeof(buff));
+			print_led(buff);
+			oldBuff = buff;
+			if(buff = 128) // key : 8
+				break;
+		}
+	} while(1);
+
+	close(dev);
+	return 0;
 }
 void print_led(unsigned char led)
 {
---
+++ p238_ledkey_array/.tmp_versions/ledkey_array_dev.mod
@@ -0,0 +1,2 @@
+/home/udooer/dd/p238_ledkey_array/ledkey_array_dev.ko
+/home/udooer/dd/p238_ledkey_array/ledkey_array_dev.o
